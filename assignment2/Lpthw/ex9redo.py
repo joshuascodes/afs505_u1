b2639d396a798bcccb0184c03d7128c2d9d45cdc
Josh <59459484+joshuascodes@users.noreply.github.com>
2020-01-22 09:10:56 -0800

    Re-worked exercise 9

---
+++ assignment2/Lpthw/ex9redo.py
@@ -0,0 +1,19 @@
+
+## This is how to make a horizontal list.
+days = "Mon Tue Wed Thur Fri Sat Sun"
+
+## This is how to make a veritcal list.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+## This is how to type as much as you want in-between the 3 quotations.
+print("""
+There's something going on here.
+with the three-double quotes.
+We can type as much as we would like.
+even more lines
+""")
+
+pwd
